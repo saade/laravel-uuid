b8801b491a0c3be16f3366a1bc5d26125cab7d2a
Ryan Chandler <ryangjchandler@gmail.com>
2022-02-08 22:35:59 +0000

    chore: use refresh instea dof lazy

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -4,12 +4,13 @@ namespace RyanChandler\Uuid\Tests;
 
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Foundation\Testing\LazilyRefreshDatabase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Support\Facades\Schema;
 use Orchestra\Testbench\TestCase as TestbenchTestCase;
 
 abstract class TestCase extends TestbenchTestCase
 {
-    use LazilyRefreshDatabase;
+    use RefreshDatabase;
 
     protected function getEnvironmentSetUp($app)
     {
